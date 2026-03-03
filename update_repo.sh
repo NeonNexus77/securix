d8ea7f1abe35e109ab5ff73eb5c90eaef95d0d39
Muntazir Ali <muntazirali2018@gmail.com>
2026-03-03 15:15:20 +0530

    chore: migrate codebase to SECURELABS repository

---
+++ update_repo.sh
@@ -0,0 +1,5 @@
+git remote set-url origin https://github.com/iammuntazirali/SECURELABS.git
+git add .
+git commit -m "chore: migrate codebase to SECURELABS repository"
+git branch -M main
+git push -u origin main
